6dcf982680d7e56de34e0a1776d91990e85ea8a7
Siarhei Tatarchanka <statarchenko@sii.pl>
2024-10-08 11:55:30 +0200

    - sm_common.hpp header updated

--- core/common/sm_common.hpp
+++ core/common/sm_common.hpp
@@ -18,30 +18,30 @@ namespace sm
 class RegisterDefinitions
 {
 public:
-    static const std::uint16_t file_control = 0;
-    static const std::uint16_t prepare_to_update = 1;
-    static const std::uint16_t app_erase = 2;
-    static const std::uint16_t record_size = 3;
-    static const std::uint16_t record_counter = 4;
-    static const std::uint16_t status = 5;
-    static const std::uint16_t gateway_buffer_size = 6;
+    static constexpr std::uint16_t file_control = 0;
+    static constexpr std::uint16_t prepare_to_update = 1;
+    static constexpr std::uint16_t app_erase = 2;
+    static constexpr std::uint16_t record_size = 3;
+    static constexpr std::uint16_t record_counter = 4;
+    static constexpr std::uint16_t status = 5;
+    static constexpr std::uint16_t gateway_buffer_size = 6;
 
     static constexpr std::uint16_t getSize() { return size; }
 
 private:
-    static const std::uint16_t size = 7;
+    static constexpr std::uint16_t size = 7;
 };
 
 class FileDefinitions
 {
 public:
-    static const std::uint16_t application = 1;
-    static const std::uint16_t metadata = 2;
+    static constexpr std::uint16_t application = 1;
+    static constexpr std::uint16_t metadata = 2;
 
     static constexpr std::uint16_t getSize() { return size; }
 
 private:
-    static const std::uint16_t size = 2;
+    static constexpr std::uint16_t size = 2;
 };
 
 } // namespace sm
